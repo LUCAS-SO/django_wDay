afa7e5e47afdf05a35ab3079110446bca5ad5792
Lucas Soria <soria.lucas@hotmail.com>
2025-05-10 01:53:01 -0300

    2° commit

--- src/app_wDay/urls.py
+++ src/app_wDay/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from .views import palabra_del_dia
 
 app_name = 'app_wDay'
 
 urlpatterns = [
-
+    path("", palabra_del_dia, name="palabra_del_dia"),
 ]
--- src/app_wDay/views.py
+++ src/app_wDay/views.py
@@ -1,3 +1,29 @@
+import requests
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+@csrf_exempt
+def palabra_del_dia(request):
+    palabra = None
+    traduccion = None
+
+    if request.method == "POST":
+        # 1) Obtener palabra aleatoria
+        resp_word = requests.get(
+            "https://random-word-api.herokuapp.com/word",
+            params={"number": 1}
+        )
+        palabra = resp_word.json()[0]  # Ej: "house"
+
+        # 2) Traducir con MyMemory
+        resp_trans = requests.get(
+            "https://api.mymemory.translated.net/get",
+            params={"q": palabra, "langpair": "en|es"}
+        )
+        data = resp_trans.json().get("responseData", {})
+        traduccion = data.get("translatedText", "")
+
+    return render(request, "base.html", {
+        "palabra": palabra,
+        "traduccion": traduccion
+    })
